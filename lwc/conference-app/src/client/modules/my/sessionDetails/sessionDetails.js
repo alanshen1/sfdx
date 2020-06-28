20ba56b86b8ff50d378cb6babf4f44190e000bd9
alphahunters <shenyifeng@shenyifengdeMacBook-Pro.local>
2020-06-28 20:42:23 +0900

    conference-app

---
+++ lwc/conference-app/src/client/modules/my/sessionDetails/sessionDetails.js
@@ -0,0 +1,22 @@
+import { LightningElement, api, track } from 'lwc';
+import { getSession } from 'data/sessionService';
+export default class sessionDetails extends LightningElement {
+    @track session;
+    @api
+    set sessionId(id) {
+        this._sessionId = id;
+        this.session = getSession(id);
+    }
+    get sessionId() {
+        return this._sessionId;
+    }
+    handleSessionsClick() {
+		  const navigateEvent = new CustomEvent('navigate', {
+		    detail: {
+		      state: 'list'
+		    }
+		  });
+		  this.dispatchEvent(navigateEvent);
+		}
+
+}
